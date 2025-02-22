02f3e0f7f0c6b21093e698fc9c34bc2cba84e733
José Son <son-rj@hotmail.com>
2025-02-22 12:02:39 -0500

    Ejemplo Arrays

--- src/main/java/com/java/practice/basics/Arrays.java
+++ src/main/java/com/java/practice/basics/Arrays.java
@@ -20,6 +20,34 @@ public class Arrays {
 
 // Ejemplo 2: Búsqueda de un elemento en un array
 public static boolean buscarElemento(String[] palabras, String palabraBuscada) {
+    //Recorrido básico de un array utilizando for tradicional
+    int[] numeros = {1, 2, 3, 4, 5};
+
+    for(int i=0;i<numeros.length; i++){
+        System.out.println("RECORRIDOS DE ARRAY: " + numeros[i]);
+    }
+
+    //For each
+    for(int numero: numeros){
+        System.out.println("recorrido con for each: " + numero);
+
+    }
+
+    int[] numeros2 = {1, 2, 3, 4, 5, 2, 4, 5, 6, 1, 2, 4};
+    Arrays.sort(numeros2);
+    Arrays.binarySearch(numeros2, 6);
+
+    int[] numeros3 = {1, 2, 3, 4, 5, 2, 4, 5, 6, 1, 2, 4};
+
+    Arrays.equals(numeros2, numeros3); //true, false
+
+    int[] copy_numeros3 = Arrays.copyOf(numeros3);
+
+    System.out.println(copy_numeros3);
+
+    
+
+
     for (String palabra : palabras) {
         if (palabra.equals(palabraBuscada)) {
             return true; // Elemento encontrado
@@ -32,8 +60,15 @@ public static boolean buscarElemento(String[] palabras, String palabraBuscada) {
 public static void main(String[] args) {
     // Ejemplo 1: Suma de elementos de un array
     int[] numeros = {1, 2, 3, 4, 5};
+
+    for(int i=0;i<numeros.length; i++){
+        System.out.println("RECORRIDOS DE ARRAY: " + numeros[i]);
+    }
     int suma = sumarElementos(numeros);
     System.out.println("La suma de los elementos del array es: " + suma);
+
+    //Metodos de ordenamiento
+    //Como podemos recorrer un array, con ciclos, ejemplo For tradicional
 
     // Ejemplo 2: Búsqueda de un elemento en un array
     String[] palabras = {"Hola", "Mundo", "Java", "Spring"};
@@ -41,5 +76,21 @@ public static void main(String[] args) {
     boolean encontrado = buscarElemento(palabras, palabraBuscada);
     System.out.println("¿La palabra corresponde '" + palabraBuscada + "' está en el array? " + encontrado);
 }
+
+    private static void sort(int[] numeros2) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void binarySearch(int[] numeros2, int i) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static int[] copyOf(int[] numeros3) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    private static void equals(int[] numeros2, int[] numeros3) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
